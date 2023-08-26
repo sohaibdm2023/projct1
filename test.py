4799e7257a8ad8680ce7a2c8f843687e44f0680e
sohaibdm2023 <sohaibjalambo@gmail.com>
2023-08-26 13:25:16 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,20 @@
+class User :
+    id=0
+    def __init__(self,name,passw):
+        self.name=name
+        self.pasw=passw
+        self.id= User.id
+        User.id+=1
+    @staticmethod
+    def user_info():
+        print(f"id method {User.id}")
+user1=User("sohaib",2020)
+User.user_info()
+print(user1.id,":",user1.name)
+user2=User("yhya",1999)
+User.user_info()
+print(user2.id,":",user2.name)
+user3=User("dada :",1000)
+User.user_info()
+print(user3.id,user3.name)
+user1.user_info()
